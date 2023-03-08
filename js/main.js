71643c2642cc7b6370adb9f4116c2c4e127cf691
guiyee <guillermodinanno@gmail.com>
2023-03-08 11:40:31 -0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let btnOpenMenu = document.querySelector("#open-menu");
+/* let btnOpenMenu = document.querySelector("#open-menu");
 let btnCloseMenu = document.querySelector("#close-menu");
 let menu = document.querySelector("#mobile-menu");
 
@@ -11,3 +11,20 @@ btnCloseMenu.addEventListener("click",() =>{
     menu.classList.add("disabled")
 })
 
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    let btnOpenMenu = document.querySelector("#open-menu");
+    let btnCloseMenu = document.querySelector("#close-menu");
+    let menu = document.querySelector("#mobile-menu");
+  
+    console.log(btnOpenMenu, btnCloseMenu, menu); // Check if elements are selected
+  
+    btnOpenMenu.addEventListener("click",() => {
+      menu.classList.remove("disabled");
+    });
+    
+    btnCloseMenu.addEventListener("click",() =>{
+      menu.classList.add("disabled");
+    });
+  });
+  
